c00039a3b5d1d8b4a68ab3011cc9b293ad98658d
007tickooayush <ayush.tickoo292000@gmail.com>
2024-04-16 15:37:17 +0530

    completed code

--- service.js
+++ service.js
@@ -19,7 +19,7 @@ const getService = async (data) => {
     //    "pageSize": 10,
     //    "totalPages": 3,
     //    "totalCount": 25,
-        return { status: 200, result, currentPage : data.currentPage, pageSize: data.pageSize, totalCount: result.count, totalPages: Math.ceil(result.count/data.pageSize)};
+        return { status: 200, result: result.result, currentPage : data.currentPage, pageSize: data.pageSize, totalCount: result.count, totalPages: Math.ceil(result.count/data.pageSize)};
     } catch (err) {
         console.log('Error occured: getService =>>',err);
         throw new Error({ status: 500, data: err.toString() });
